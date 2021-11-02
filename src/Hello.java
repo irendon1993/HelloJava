735044e636ae928f3c531c13b1ceaf6cdf305378
irendon1993 <isaacrendon7@gmail.com>
2021-11-02 13:44:42 -0400

    Just another hello world

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World Again");
-        System.out.println("Testing ");
+        System.out.println("Testing");
     }
 }
